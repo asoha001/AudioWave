405d25fff4313c23453d8923176fbf555422850b
selina <134965575+selinakairyte@users.noreply.github.com>
2025-01-08 17:35:36 +0000

    .

--- script.js
+++ script.js
@@ -35,3 +35,24 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+// audio and volume slider functionality
+const audio = document.getElementById('audio-player');
+const lyrics = document.querySelectorAll('.lyrics-container p');
+const volumeSlider = document.getElementById('volumeRange');
+
+// Highlight the current lyric based on playback time
+audio.addEventListener('timeupdate', () => {
+const currentTime = audio.currentTime;
+
+lyrics.forEach((lyric) => {
+const lyricTime = parseFloat(lyric.getAttribute('data-time'));
+if (lyricTime <= currentTime) {
+lyrics.forEach((l) => l.classList.remove('highlight'));
+lyric.classList.add('highlight');
+}
+});
+});
+volumeSlider.addEventListener('input', (e) => {
+const volume = e.target.value;
+audio.volume = volume; // Set the audio player's volume
+});
